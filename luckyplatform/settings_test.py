f34a947582ae1723e21c7ce37e416d2ca38db205
leechannl <leechannl@icloud.com>
2017-01-25 16:54:08 +0800

    fix (setting): add test env settings

--- luckyplatform/settings_test.py
+++ luckyplatform/settings_test.py
@@ -1,56 +1,91 @@
 # -*- coding: utf-8 -*-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ROOT_URL = 'http://121.41.6.238:8000'
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+COUNTRY = 'vn'
+
+CITY_LAN = 'vn'
+
+LANGUAGE_CODE = 'en-us'
+
+ROOT_URL = 'http://121.40.29.144'
 
 CELERY_BROKER = 'redis://127.0.0.1:6379//'
 
+GEOLITE_CITY_DB = '/home/ubuntu/af-env/data/GeoLite2-City.mmdb'
+
+LIST_UPDATE = '/home/ubuntu/af-env/luckyservice/tools/data/online_test/list_update1'
+
+WEB_APP_ROOT_URL = 'http://121.40.29.144:9898'
+
+ADMIN_ROOT_URL = 'http://121.40.29.144/'
+
+INVITER_SHARE_LINK = 'http://121.40.29.144:9898/share_app/'
+
+APPLE_TIDS = [123, 117, 138, 118, 121, 122, 124, 125]
+
+RAVEN_CONFIG = {
+    'dsn': 'http://eb86dcf4383342e986a991ba3df94341:ef5ea26ffd054f2e8ea9f5887ab6cdde@114.55.27.19:9000/3',
+}
+
+MONGO_ADDR = "mongodb://127.0.0.1/"
+
+# FIXME: NOT SAFE
+CORS_ORIGIN_ALLOW_ALL = True
+
 MYSQL_CONF = {
     'db': 'mysql://root:123456@127.0.0.1:3306/lucky?charset=utf8',
-    'DEBUG': DEBUG
+    'DEBUG': False
 }
 
 ADMIN_CONF = {
     'db': 'mysql://root:123456@127.0.0.1:3306/admin?charset=utf8',
-    'DEBUG': DEBUG
+    'DEBUG': False
 }
-
-ZK_HOSTS = '172.31.1.71:2181,172.31.1.99:2182,172.31.1.99:2183,172.31.1.71:2184,172.31.1.71:2185'
-
-ZK_PROXY_DIR = '/zk/codis/db_test/proxy'
 
 DEBUG_USER = 1
 
 SERVICE_ID = 1
 
-# PingPlusPlus config
-
-# Pingpp App key
-API_KEY = 'sk_live_KuPGuHmDuvr5qTyrbDaz180G'
-
-# Pingpp alipay wap return and cancel URL
-SUCCESS_URL = 'http://www.yourdomain.com/success'
-CANCEL_URL = 'http://www.yourdomain.com/cancel'
-
-# iapppay notify URL
-IAPPPAY_NOTIFY_URL = 'http://121.41.6.238/api/v1/pay/iapppay/notify/'
-
-# ipaynow notify URL
-IPAYNOW_NOTIFY_URL = 'http://121.41.6.238/api/v1/pay/ipaynow/notify/'
-
 FRESH_RECOMMEND = [129, 122]
 
 GOD_TIDS = [129]
 
-SPRING_GROUP = [{
-    'gid': 125, 'cover': '', 'title': '',
-},{
-    'gid': 129, 'cover': '', 'title': '',
-},{
-    'gid': 133, 'cover': '', 'title': '',
-},{
-    'gid': 143, 'cover': '', 'title': '',
-}]
-
 ANNOUNCE_DELAY = 30
+
+# paypal settings
+PAYPAL_REDIRECT_URL = "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=%s"
+PAYPAL_API_USERNAME = "sunshineking86-facilitator_api1.gmail.com"
+PAYPAL_API_PASSWORD = "WR4ZHKZNWHPSR65D"
+PAYPAL_API_SIGNATURE = "AFcWxV21C7fd0v3bYYYRCpSSRl31AbDl8KV.UJSCOn1uuHTE0e1Vs3R0"
+PAYPAL_API_ENVIRONMENT = 'SANDBOX'
+
+PAYPAL_RETURN_URL = 'http://121.40.29.144/api/v1/pay/paypal/return'
+PAYPAL_CANCEL_URL = 'http://121.40.29.144/api/v1/pay/paypal/cancel'
+PAYPAL_NOTIFY_URL = 'http://121.40.29.144/api/v1/pay/paypal/notify'
+
+# NganLuong pay
+NGANLUONG_CHECK_ORDER_URL = 'http://121.40.29.144/api/v1/pay/nganluong/check_order'
+NGANLUONG_RETURN_URL = 'http://121.40.29.144/api/v1/pay/nganluong/return'
+NGANLUONG_NOTIFY_URL = 'http://121.40.29.144/api/v1/pay/nganluong/notify'
+NGANLUONG_CLOSE_URL = 'http://121.40.29.144/api/v1/pay/close_webview'
+
+# Coda pay
+CODA_PAY_GATEWAY_URL = 'http://121.40.29.144/api/v1/pay/coda_pay/gateway/%s/'
+CODA_API_KEY_DICT = {
+    'TWD': '66180937d31a768a62bf463b3d5e5547',
+    'IDR': 'f0e5a52f4171b6d4d6b1a1b5b77b3ad0',
+}
+
+# Fortumo pay
+FORTUMO_SERVICE_ID = '964f5dc6556ef062e2d6883ef94908e6'
+FORTUMO_SECRET = 'efd5746d7a21224c6e9fa95beb9c95bf'
+FORTUMO_NOTIFY_URL = 'http://121.40.29.144/api/v1/pay/fortumo/notify/'
+FORTUMO_CLOSE_URL = 'http://121.40.29.144/api/v1/pay/close_webview/'
+
+# Precard
+PRECARD_GATEWAY_URL = 'http://121.40.29.144/api/v1/pay/precard/%s/'
+PAY_GATEWAY_STATUS_URL = 'http://121.40.29.144/api/v1/pay/%s/status'
