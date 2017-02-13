fcb2c9d69100a31f868ba4f32ac621e0df5f8665
leechannl <leechannl@icloud.com>
2017-02-13 18:33:20 +0800

    feat (tool): update stats paid user script

--- tools/stats_paid_users.py
+++ tools/stats_paid_users.py
@@ -12,6 +12,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'luckyplatform.settings'
 from luckycommon.model import orm
 from luckycommon.account.model.account import Account
 from luckycommon.model.transaction import Transaction, TRANSACTION_TYPE, TRANSACTION_STATUS
+from luckycommon.order.model.order import AwardedOrder
+from luckycommon.model.activity import Activity
 
 paid_user_query_result = orm.session.query(Transaction.user_id).filter(Transaction.type == TRANSACTION_TYPE.IN).filter(
     Transaction.status == TRANSACTION_STATUS.DONE).distinct().all()
@@ -23,9 +25,10 @@ for items in paid_user_query_result:
         'user_id': None,
         'register_at': None,
         'first_recharge_at': None,
-        'first_recharge_amount': None,
+        'first_recharge_amount': 0,
         'last_recharge_at': None,
-        'total_recharge_amount': None
+        'total_recharge_amount': 0,
+        'total_award_amount': 0
     }
     user_id = int(items[0])
     account = Account.query.filter(Account.id == user_id).first()
@@ -41,9 +44,14 @@ for items in paid_user_query_result:
     result_dict['total_recharge_amount'] = float(
         orm.session.query(func.sum(Transaction.price)).filter(Transaction.user_id == user_id).filter(
             Transaction.type == TRANSACTION_TYPE.IN).filter(Transaction.status == TRANSACTION_STATUS.DONE).first()[0])
+    for award_item in AwardedOrder.query.filter(AwardedOrder.user_id == user_id).all():
+        activity = Activity.query.filter(Activity.id == award_item.activity_id).first()
+        if activity:
+            result_dict['total_award_amount'] += activity.target_amount
+
     result_list.append(result_dict)
 
 for item in result_list:
-    print('%s,%s,%s,%s,%s,%s' % (
+    print('%s,%s,%s,%s,%s,%s,%s' % (
         item['user_id'], item['register_at'], item['first_recharge_at'], item['first_recharge_amount'], item['last_recharge_at'], item[
-            'total_recharge_amount']))
+            'total_recharge_amount'], item['total_award_amount']))
