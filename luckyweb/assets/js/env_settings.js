17e69e060614c10298d56a2dbac0e39ccae18905
leechannl <leechannl@icloud.com>
2017-02-14 16:12:51 +0800

    fix (qiniu): fix qiniu domain

--- luckyweb/assets/js/env_settings.js
+++ luckyweb/assets/js/env_settings.js
@@ -1,4 +1,4 @@
 window.QINIU_DOMAIN = 'http://p.lucky-gou.com/';
 window.PAGE_TITLE = '一元购印尼版<b>生产环境</b>控制台';
 window.AVATAR_DOMAIN = 'http://a.lucky-gou.com/';
-window.UPTOKEN_URL = '/admin/uptoken?bucket=avatar';
+window.UPTOKEN_URL = '/admin/uptoken';
