4ecb9e77a7147a961f203e73d5f7b168350ed1d1
leechannl <leechannl@icloud.com>
2017-02-22 15:40:39 +0800

    fix (sensor): make sensor close by default

--- luckyplatform/env_settings.py
+++ luckyplatform/env_settings.py
@@ -69,7 +69,7 @@ COIN_TIDS = {
     #    646: 1000,
 }
 
-GP_FLAG = True  # GP switch, True is on sensor
+GP_FLAG = False  # GP switch, True is on sensor
 GP_VERSION_CODE = 130
 GP_ALLOWED_TIDS = [752, 753, 754, 755, 750, 751, 665]
 
