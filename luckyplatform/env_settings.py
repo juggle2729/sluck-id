765cf1526bc5731a89a79bf65053fe79e16bbb7e
leechannl <leechannl@icloud.com>
2017-02-27 14:59:31 +0800

    feat (strategy): add one more virtual goods

--- luckyplatform/env_settings.py
+++ luckyplatform/env_settings.py
@@ -57,7 +57,7 @@ SHOW_BANNED_TIDS = []
 MISS_RETURN_TID = 641
 
 # virtual goods IDs, use special strategy
-VIRTUAL_GOODS_IDS = [796, 668, 667, 794, 802]
+VIRTUAL_GOODS_IDS = [796, 668, 667, 794, 802, 856]
 
 COIN_TIDS = {
     #    659: 5,
