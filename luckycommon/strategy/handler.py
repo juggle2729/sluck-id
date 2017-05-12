9f2180f0f2297a9066cfd8fd5cd409a7d6b8b5ba
aaa <aaa@bbb>
2017-05-12 16:35:35 +0800

    fix text in db return unicode convert to str

--- luckycommon/strategy/handler.py
+++ luckycommon/strategy/handler.py
@@ -248,8 +248,8 @@ def get_candidate_win_users(activity):
 
 def get_user_activity_weight(user_id, activity):
     numbers_chars = orm.session.query(UserActivity.numbers).filter(
-        UserActivity.activity_id == activity.id, user_id ==user_id).first()
-    number_len = numbers_chars.count(",")+1
+        UserActivity.activity_id == activity.id, UserActivity.user_id ==user_id).first()
+    number_len = str(numbers_chars).count(",")+1
     total_amount = activity.target_amount
     return float(number_len)
 
