c813774ce2abecf950d3dbe913a46126d1b9a911
leechannl <leechannl@icloud.com>
2017-06-19 16:33:46 +0800

    feat(pay): open recharge card pay type

--- luckyapi/views/transaction.py
+++ luckyapi/views/transaction.py
@@ -143,7 +143,7 @@ def filter_available_pay_types(pay_types, platform, version_code, locale, chn):
         ]
     if platform == 'android' and 136 <= int(version_code) and locale == 'id':
         return [
-            # pay_types[PayType.SELF_RECHARGE_CARD.value],
+            pay_types[PayType.SELF_RECHARGE_CARD.value],
             pay_types[PayType.MIMO_INDOSAT.value],
             pay_types[PayType.MIMO_THREE.value],
             pay_types[PayType.MIMO_XL.value],
@@ -167,7 +167,7 @@ def filter_available_pay_types(pay_types, platform, version_code, locale, chn):
         ]
     if platform == 'ios' and locale == 'id':
         return [
-            # pay_types[PayType.SELF_RECHARGE_CARD.value],
+            pay_types[PayType.SELF_RECHARGE_CARD.value],
             pay_types[PayType.MIMO_INDOSAT.value],
             pay_types[PayType.MIMO_THREE.value],
             pay_types[PayType.MIMO_XL.value],
